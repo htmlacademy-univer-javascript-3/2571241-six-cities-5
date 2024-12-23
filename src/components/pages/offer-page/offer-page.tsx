7562a2c6d67de8a3969d039a607501fbda1eab9d
VartoS <ngaseneger@gmail.com>
2024-12-23 22:49:59 +0500

    Сделана галерея и инфа про offer

--- src/components/pages/offer-page/offer-page.tsx
+++ src/components/pages/offer-page/offer-page.tsx
@@ -1,123 +1,103 @@
 import { useParams } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../../store/hooks';
-import { getReviews, getSingleOffer } from '../../../store/single-offer-data-process/single-offer-data-process.selectors';
+import {
+  getReviews,
+  getSingleOffer,
+  getSingleOfferDataLoadingStatus,
+} from '../../../store/single-offer-data-process/single-offer-data-process.selectors';
 import { Header } from '../main-page/header';
 import CommentForm from './comment-form';
 import { ReviewList } from './review-list';
 import { useEffect } from 'react';
 import { fetchSingleOfferAction } from '../../../store/api-actions';
+import { OfferGallery } from './offer-gallery';
+import { LoadingScreen } from '../loading-page/loading-page';
 
 function OfferPage(): JSX.Element {
-  const offerId = useParams<{id: string}>().id as string;
+  const offerId = useParams<{ id: string }>().id as string;
   const dispatch = useAppDispatch();
   useEffect(() => {
-    dispatch(fetchSingleOfferAction({offerId}))
-  }, [offerId, dispatch])
-
+    dispatch(fetchSingleOfferAction({ offerId }));
+  }, [offerId, dispatch]);
 
   const reviews = useAppSelector(getReviews);
+  const currentOffer = useAppSelector(getSingleOffer);
+  const isDataStillLoading = useAppSelector(getSingleOfferDataLoadingStatus);
+  if (!currentOffer || isDataStillLoading) {
+    return <LoadingScreen />;
+  }
   return (
     <div className="page">
-      <Header/>
+      <Header />
       <main className="page__main page__main--offer">
         <section className="offer">
-          <div className="offer__gallery-container container">
-            <div className="offer__gallery">
-              <div className="offer__image-wrapper">
-                <img
-                  className="offer__image"
-                  src="img/room.jpg"
-                  alt="Photo studio"
-                />
-              </div>
-              <div className="offer__image-wrapper">
-                <img
-                  className="offer__image"
-                  src="img/apartment-01.jpg"
-                  alt="Photo studio"
-                />
-              </div>
-              <div className="offer__image-wrapper">
-                <img
-                  className="offer__image"
-                  src="img/apartment-02.jpg"
-                  alt="Photo studio"
-                />
-              </div>
-              <div className="offer__image-wrapper">
-                <img
-                  className="offer__image"
-                  src="img/apartment-03.jpg"
-                  alt="Photo studio"
-                />
-              </div>
-              <div className="offer__image-wrapper">
-                <img
-                  className="offer__image"
-                  src="img/studio-01.jpg"
-                  alt="Photo studio"
-                />
-              </div>
-              <div className="offer__image-wrapper">
-                <img
-                  className="offer__image"
-                  src="img/apartment-01.jpg"
-                  alt="Photo studio"
-                />
-              </div>
-            </div>
-          </div>
+          <OfferGallery />
           <div className="offer__container container">
             <div className="offer__wrapper">
-              <div className="offer__mark">
-                <span>Premium</span>
-              </div>
+              {currentOffer.isPremium && (
+                <div className="offer__mark">
+                  <span>Premium</span>
+                </div>
+              )}
               <div className="offer__name-wrapper">
-                <h1 className="offer__name">
-                  Beautiful &amp; luxurious studio at great location
-                </h1>
-                <button className="offer__bookmark-button button" type="button">
-                  <svg className="offer__bookmark-icon" width={31} height={33}>
-                    <use xlinkHref="#icon-bookmark" />
+                <h1 className="offer__name">{currentOffer.title}</h1>
+                <button
+                  className={`offer__bookmark-button${
+                    currentOffer.isFavorite ? '--active' : ''
+                  } button`}
+                  type="button"
+                >
+                  <svg className="offer__bookmark-icon" width="31" height="33">
+                    <use xlinkHref="#icon-bookmark"></use>
                   </svg>
-                  <span className="visually-hidden">To bookmarks</span>
+                  {currentOffer.isFavorite ? (
+                    <span className="visually-hidden">In bookmarks</span>
+                  ) : (
+                    <span className="visually-hidden">To bookmarks</span>
+                  )}
                 </button>
               </div>
               <div className="offer__rating rating">
                 <div className="offer__stars rating__stars">
-                  <span style={{ width: '80%' }} />
+                  <span
+                    style={{
+                      width: `${Math.round(currentOffer.rating) * 20}%`,
+                    }}
+                  ></span>
                   <span className="visually-hidden">Rating</span>
                 </div>
-                <span className="offer__rating-value rating__value">4.8</span>
+                <span className="offer__rating-value rating__value">
+                  {currentOffer.rating}
+                </span>
               </div>
               <ul className="offer__features">
                 <li className="offer__feature offer__feature--entire">
-                  Apartment
+                  {String(currentOffer.type).charAt(0).toUpperCase() +
+                    String(currentOffer.type).slice(1)}
                 </li>
                 <li className="offer__feature offer__feature--bedrooms">
-                  3 Bedrooms
+                  {`${currentOffer.bedrooms} Bedroom${
+                    currentOffer.bedrooms > 1 ? 's' : ''
+                  }`}
                 </li>
                 <li className="offer__feature offer__feature--adults">
-                  Max 4 adults
+                  {`Max ${currentOffer.maxAdults} Adult${
+                    currentOffer.maxAdults > 1 ? 's' : ''
+                  }`}
                 </li>
               </ul>
               <div className="offer__price">
-                <b className="offer__price-value">€120</b>
+                <b className="offer__price-value">€{currentOffer.price}</b>
                 <span className="offer__price-text">&nbsp;night</span>
               </div>
               <div className="offer__inside">
                 <h2 className="offer__inside-title">What&apos;s inside</h2>
                 <ul className="offer__inside-list">
-                  <li className="offer__inside-item">Wi-Fi</li>
-                  <li className="offer__inside-item">Washing machine</li>
-                  <li className="offer__inside-item">Towels</li>
-                  <li className="offer__inside-item">Heating</li>
-                  <li className="offer__inside-item">Coffee machine</li>
-                  <li className="offer__inside-item">Baby seat</li>
-                  <li className="offer__inside-item">Kitchen</li>
-                  <li className="offer__inside-item">Dishwasher</li>
-                  <li className="offer__inside-item">Cabel TV</li>
-                  <li className="offer__inside-item">Fridge</li>
+                  {currentOffer.goods.map((good) => (
+                    <li className="offer__inside-item" key={`good-${good}`}>
+                      {good}
+                    </li>
+                  ))}
                 </ul>
               </div>
               <div className="offer__host">
@@ -126,33 +106,27 @@ function OfferPage(): JSX.Element {
                   <div className="offer__avatar-wrapper offer__avatar-wrapper--pro user__avatar-wrapper">
                     <img
                       className="offer__avatar user__avatar"
-                      src="img/avatar-angelina.jpg"
+                      src={currentOffer.host.avatarUrl}
                       width={74}
                       height={74}
                       alt="Host avatar"
                     />
                   </div>
-                  <span className="offer__user-name">Angelina</span>
-                  <span className="offer__user-status">Pro</span>
+                  <span className="offer__user-name">{currentOffer.host.name}</span>
+                  {currentOffer.host.isPro && <span className="offer__user-status">Pro</span>}
                 </div>
                 <div className="offer__description">
                   <p className="offer__text">
-                    A quiet cozy and picturesque that hides behind a a river by
-                    the unique lightness of Amsterdam. The building is green and
-                    from 18th century.
-                  </p>
-                  <p className="offer__text">
-                    An independent House, strategically located between Rembrand
-                    Square and National Opera, but where the bustle of the city
-                    comes to rest in this alley flowery and colorful.
+                    {currentOffer.description}
                   </p>
                 </div>
               </div>
               <section className="offer__reviews reviews">
                 <h2 className="reviews__title">
-                  Reviews · <span className="reviews__amount">{reviews.length}</span>
+                  Reviews ·{' '}
+                  <span className="reviews__amount">{reviews.length}</span>
                 </h2>
-                <ReviewList reviews={reviews}/>
+                <ReviewList reviews={reviews} />
                 <CommentForm />
               </section>
             </div>
